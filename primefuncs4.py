aa7e611a648bbbcc68030526d36810b43e6a334f
Zadok <119855051+Zadok-01@users.noreply.github.com>
2023-04-27 15:32:31 +0100

    Update primefuncs4.py

--- primefuncs4.py
+++ primefuncs4.py
@@ -79,7 +79,7 @@ def is_prime(n):
 		return n > 1
 	if not n % 2 or not n % 3:
 		return False
-	for i in range(5, int(n ** 0.5) + 1, 6):
+	for i in range(5, insqrt(n) + 1, 6):
 		if not n % i or not n % (i + 2):
 			return False
 	return True
